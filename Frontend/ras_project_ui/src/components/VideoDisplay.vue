2367ff8495bfa4d57b14433042b86f2be89cf125
hanyongjun <gksdydwns34@naver.com>
2023-11-22 18:29:33 +0900

    send.py 실행 후 웹 페이지에 시작버튼을 누르면 카메라 작동 시작

--- Frontend/ras_project_ui/src/components/VideoDisplay.vue
+++ Frontend/ras_project_ui/src/components/VideoDisplay.vue
@@ -1,44 +1,71 @@
 <template>
- 
-    <div id="imageContainer">
-      <img id="liveImage" :src="videoSource" :style="{ transform: `scale(${zoomLevel})` }" alt="Live Image">
-    </div>
+  <div id="imageContainer">
+    <table>
+      <tr>
+        <td>
+          <div class="rectangle">
+            <img id="liveImage" :src="isStreaming ? videoSource : ''" alt="Live Image">
+          </div>
+        </td>
+        <td>
+          <div class="rectangle" id="serve_img">
 
-    <div class="pm_btn">
-    <button @click="zoomIn">확대</button>
-    <button @click="zoomOut">축소</button>
+        </div>
+      </td>
+
+      </tr>
+      <button @click="startStreaming" id="camera_start_btn">Start Streaming</button>
+      <tr>
+        <td>
+          <div class="rectangle2" id="user_it">
+            <div id="user_input">
+            <input type="text" placeholder="이름">
+            <input type="text" placeholder="전화번호">
+            <input type="date">
+          </div>
+        </div>
+      </td>
+        
+      <td>
+        <div class="rectangle2" id="log_it">
+   
+          <div id="footer"></div>
+      </div>
+     </td>
+
+      </tr>
+    </table>
   </div>
+  <!-- 생략... -->
 </template>
 
 <script>
-import websocket from '../usedata/websocket';
+import websocket from '../usedata/websocket'; 
 import '../css/main.css';
 
 export default {
   data() {
     return {
       videoSource: '',
-      zoomLevel: 1.0,
+      isStreaming: false,
+      ws: null,
     };
   },
+
   methods: {
-    zoomIn() {
-      if (this.zoomLevel < 2.0) {
-        this.zoomLevel += 0.1;
-      }
+    startStreaming() {
+      this.isStreaming = true;
+      this.ws = websocket.initWebSocket();
+
+      this.ws.onmessage = event => {
+        const blob = new Blob([event.data], { type: 'image/jpeg' });
+        this.videoSource = URL.createObjectURL(blob);
+      };
+
+      this.ws.onclose = event => {
+        console.error("WebSocket closed:", event);
+      };
     },
-    zoomOut() {
-      if (this.zoomLevel > 0.5) {
-        this.zoomLevel -= 0.1;
-      }
-    },
-  },
-  mixins: [websocket],
-  mounted() {
-    this.initWebSocket();
   },
 };
 </script>
-
-
-  
