aa90e844db78feef5074b3d5f6fdb3c428f0e2a7
hanyongjun <gksdydwns34@naver.com>
2023-11-28 11:48:09 +0900

    localhost주소 변경변수를 할당 후 상위폴더에 저장해서  포트번호 숨김

--- Frontend/ras_project_ui/src/components/VideoDisplay.vue
+++ Frontend/ras_project_ui/src/components/VideoDisplay.vue
@@ -28,8 +28,16 @@
       </td>
 
       <td>
-        <div class="rectangle2" id="log_it">
-   
+       <div class="rectangle2" id="log_it">
+        <div>
+          <div v-for="log in logs" :key="log.seq">
+            <p>{{ log.administrator }}</p>
+            <p>{{ log.phone }}</p>
+            <p>{{ log.logtime }}</p>
+            <p>{{ log.logpath }}</p>
+            <p>{{ log.smsflag }}</p>
+      </div>
+     </div>
           <div id="footer"></div>
       </div>
      </td>
@@ -43,6 +51,8 @@
 <script>
 import websocket from '../usedata/websocket'; 
 import '../css/main.css';
+import axios from 'axios';
+
 
 export default {
   data() {
@@ -50,9 +60,9 @@ export default {
       videoSource: '',
       isStreaming: false,
       ws: null,
+      logs: [],  // logs 배열 추가
     };
   },
-
   methods: {
     startStreaming() {
       this.isStreaming = true;
@@ -67,6 +77,10 @@ export default {
         console.error("WebSocket closed:", event);
       };
     },
+  },
+  async created() {  // created 라이프사이클 훅에서 데이터 가져오기
+    const response = await axios.get('http://10.200.43.222:23241/docs#/default/logs_api_v1_logs_get');
+    this.logs = response.data;
   },
 };
 </script>
